bd94f191fb81970ace4cc7032adc7625a531cd35
Samū Ketuvo <onankman@gmail.com>
2025-06-09 15:29:01 +0200

    Document PredicateInddex

--- src/nf/index.rs
+++ src/nf/index.rs
@@ -10,40 +10,61 @@ pub struct PredicateIndex {
     preds: BTreeMap<Name, BTreeSet<Vec<AExpr>>>
 }
 
+/// A predicate index is a set of predicate calls. That is, it stores elements like
+/// `P(:x), P(a), Q(:x, :y), Q(a, :z)`. The index indexes these sets by the occurences
+/// of predicates, that is, instead of just storing a set of predicates, it keeps
+/// the different sets of arguments that were given to each predicate:
+/// ```txt
+/// P -> :x
+///      a
+/// Q -> :x, :y
+///      a,  :z
+/// ```
+/// 
+/// The advantage of this representation is that one can quickly find all the occurences
+/// of a specific predicate, and one can find the distinct predicate names that occur
+/// in the set. When attempting to unify two clauses, this is particularly useful.
 impl PredicateIndex {
+    /// Creates a new [PredicateIndex].
     pub fn new() -> Self {
         Self {
             preds: BTreeMap::new()
         }
     }
 
+    /// Clears this [PredicateIndex].
     pub fn clear(&mut self) {
         self.preds.clear();
     }
 
+    /// Inserts a new [Atom] into the index.
     pub fn insert(&mut self, atom: Atom) -> bool {
         match atom {
             Atom::Pred(name, args) => self.insert_pred(name, args)
         }
     }
 
+    /// Removes an [Atom] from the index.
     pub fn remove(&mut self, atom: &Atom) -> bool {
         match atom {
             Atom::Pred(name, args) => self.remove_pred(name, args)
         }
     }
 
+    /// Tests whether the given [Atom] is in the index.
     pub fn contains(&self, atom: &Atom) -> bool {
         match atom {
             Atom::Pred(name, args) => self.contains_pred(name, args)
         }
     }
 
+    /// Inserts a new predicate into the index, given by a [Name] and a [Vec] of [AExpr]s.
     pub fn insert_pred(&mut self, pred: Name, args: Vec<AExpr>) -> bool {
         let set = self.preds.entry(pred).or_insert_with(|| BTreeSet::new());
         set.insert(args)
     }
 
+    /// Removes a predicate from the index, given a [Name] and a [Vec] of [AExpr]s.
     pub fn remove_pred(&mut self, pred: &Name, args: &Vec<AExpr>) -> bool {
         let mut empty = false;
         let mut rmv = false;
@@ -60,6 +81,7 @@ impl PredicateIndex {
         rmv
     }
 
+    /// Tests whether a predicate is in the index, given a [Name] and a [Vec] of [AExpr]s.
     pub fn contains_pred(&self, pred: &Name, args: &Vec<AExpr>) -> bool {
         if let Some(r) = self.preds.get(pred) {
             r.contains(args)
@@ -68,6 +90,7 @@ impl PredicateIndex {
         }
     }
 
+    /// Removes all predicates from the index with a given name.
     pub fn remove_preds(&mut self, pred: &Name) -> bool {
         if let Some(r) = self.preds.remove(pred) {
             !r.is_empty()
@@ -76,19 +99,25 @@ impl PredicateIndex {
         }
     }
 
+    /// Gets all occurences of a predicate given by name. The returned set only lists the
+    /// different combinations of arguments that appear in the index.
     pub fn get_preds(&self, pred: &Name) -> Option<&BTreeSet<Vec<AExpr>>> {
         self.preds.get(pred).filter(|set| !set.is_empty())
     }
 
+    /// Tests whether any predicate with the given name exists in this index.
     pub fn contains_preds(&self, pred: &Name) -> bool {
         self.preds.get(pred).filter(|set| !set.is_empty()).is_some()
     }
     
 
+    /// Returns an [Iterator] that iterates the distinct predicate names that occur
+    /// in this index.
     pub fn iter_pred_names(&self) -> impl Iterator<Item = &Name> {
-        self.preds.keys()
+        self.preds.iter().filter(|(_, v)| !v.is_empty()).map(|(k, _)| k)
     }
 
+    /// Retrns an [Iterator] that iterates all predicates in the index.
     pub fn iter_preds(&self) -> impl Iterator<Item = (Name, &Vec<AExpr>)> {
         self.preds.iter().flat_map(|(name, set)| {
             set.iter().map(|args| (*name, args))
@@ -97,6 +126,8 @@ impl PredicateIndex {
 
 
 
+    /// Computes the union of this [PredicateIndex] and another. The returned
+    /// index has all predicates that occur in both this set and the other.
     pub fn union(mut self, other: Self) -> Self {
         for (name, set) in other.preds.into_iter() {
             for args in set {
@@ -107,10 +138,13 @@ impl PredicateIndex {
         self
     }
 
+    /// Tests whether this [PredicateIndex] contains no predicates.
     pub fn is_empty(&self) -> bool {
         self.preds.is_empty() || self.preds.values().all(|it| it.is_empty())
     }
 
+    /// Tests whether this [PredicateIndex] is disjoint from another. This
+    /// is true if no predicate in this indx appears in the other, and vice versa.
     pub fn is_disjoint(&self, other: &Self) -> bool {
         for (name, set) in other.preds.iter() {
             if let Some(own_set) = self.get_preds(name) {
