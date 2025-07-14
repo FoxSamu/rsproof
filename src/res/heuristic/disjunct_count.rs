15b20540bacf8e40961dbe765b709e6971db0503
Samū Ketuvo <onankman@gmail.com>
2025-07-14 18:35:28 +0200

    Fix some warnings

--- src/res/heuristic/disjunct_count.rs
+++ src/res/heuristic/disjunct_count.rs
@@ -1,14 +1,13 @@
-use crate::expr::AExpr;
 use crate::nf::Clause;
 
 pub fn disjunct_count(c: &Clause) -> u64 {
     let mut heuristic = 0u64;
 
-    for pred in c.pos().iter_preds() {
+    for _ in c.pos().iter_preds() {
         heuristic += 1;
     }
 
-    for pred in c.neg().iter_preds() {
+    for _ in c.neg().iter_preds() {
         heuristic += 1;
     }
     
